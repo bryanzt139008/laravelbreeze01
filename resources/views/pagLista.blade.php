4c0a0c370bef83d8d7c0507c3d5a140593bf39cb
Bryan Zapana <bryanbozt139008@gmail.com>
2023-11-22 19:28:20 -0500

    backup

---
+++ resources/views/pagLista.blade.php
@@ -0,0 +1,10 @@
+@extends('pagPlantilla')
+
+@section('titulo')
+   <h1> Pagina lista </h1>
+@endsection
+
+@section('seccion')
+    <h1> Lista </h1>
+@endsection
+   
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,28 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+Route::get('/', [PagesController::class, 'fnIndex'])-> name ('xInicio');
+
+
+Route::get('/galeria/{numero}',[PagesController::class,'fnGaleria'])-> where('numero','[0-9')->name('xGaleria');
+
+Route::get('/lista',[PagesController::class,'fnLista']) ->name('xLista');
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+Route::get('/galeria/{numero}', function ($numero) {
+    return "Este es el codigo de la foto: ".$numero;
+}) -> where('numero', '[0-9]+');
+
+Route::view('/galeria',('/pagGaleria'),['valor'=>15]) ->name('xGaleria');
+
+
+Route::get('/lista',function () {
+    return view('pagLista');
+})->name('xLista');
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,3 +36,26 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+}) -> name('xInicio');
+*/
+
+/*
+Route::get('/saludo', function(){
+    return "Hola mundo desde laravel";
+});
+*/
